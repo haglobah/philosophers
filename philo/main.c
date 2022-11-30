f15a69614c222117584ea29b84eec7e377490725
Beat Hagenlocher <bah@posteo.de>
2022-11-30 21:17:17 +0100

    Fix Segfault on wrong args

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:26:38 by bhagenlo          #+#    #+#             */
-/*   Updated: 2022/11/30 19:04:06 by bhagenlo         ###   ########.fr       */
+/*   Updated: 2022/11/30 21:16:43 by bhagenlo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	main(int argc, char *argv[])
 	t_phi	*p;
 
 	p = parse(argc, argv);
+	if (p == NULL)
+		return (1);
 	run_philos(p);
 	del_phis(p);
 	return (0);
